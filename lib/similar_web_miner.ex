3fe119e069d74e5eda22b76ba029fe86ebef52b7
RaphSfeir <raph.sfeir@gmail.com>
2020-05-13 18:31:44 +0100

    End date by default from last month

--- lib/similar_web_miner.ex
+++ lib/similar_web_miner.ex
@@ -12,7 +12,7 @@ defmodule SimilarWebMiner do
   """
   def total_visits_last_months(domain) do
     today = Date.utc_today()
-    end_date = get_first_of_month(today)
+    end_date = get_first_of_month(Date.add(today, -1 * 30))
     start_date = get_first_of_month(Date.add(today, -6 * 30))
     total_visits(domain, start_date, end_date, "monthly")
   end
