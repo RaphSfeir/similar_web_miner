262349a0b57c140364187dbba74fd457665eada5
RaphSfeir <raph.sfeir@gmail.com>
2020-06-04 17:19:24 +0100

    Bump version to 0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimilarWebMiner.MixProject do
   def project do
     [
       app: :similar_web_miner,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       package: package(),
