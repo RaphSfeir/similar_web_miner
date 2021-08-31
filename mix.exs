134a607ab4f575f089f3548db6be02996192a567
Raphaël Sfeir <raph.sfeir@gmail.com>
2021-08-31 14:23:05 +0100

    Bump version, breaking change

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimilarWebMiner.MixProject do
   def project do
     [
       app: :similar_web_miner,
-      version: "0.1.9",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       package: package(),
